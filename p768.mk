e49a4cad43d2d1b16614a69e8f137d1091c0ac60
art1p <art1p@hotmail.com>
2013-11-15 16:47:38 +0000

    Update to cm-10.2

--- p768.mk
+++ p768.mk
@@ -18,8 +18,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init.p768.usb.rc:root/init.p768.usb.rc \
     $(LOCAL_PATH)/init.lgep768board.rc:root/init.lgep768board.rc \
     $(LOCAL_PATH)/ueventd.lgep768board.rc:root/ueventd.lgep768board.rc \
-    $(LOCAL_PATH)/fstab.u2:root/fstab.u2 \
-    $(LOCAL_PATH)/vold.fstab:system/etc/vold.fstab
+    $(LOCAL_PATH)/fstab.u2:root/fstab.lgep768board
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
